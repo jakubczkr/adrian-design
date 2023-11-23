ebc41cf8a2db55c9d3beb3dcc048c4fb3cd06482
Rafał Jakubczyk <112645697+jakubczkr@users.noreply.github.com>
2023-11-23 21:37:23 +0100

    Update

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function showMainContent() {
+  document.getElementById("content-div").style.display = "flex"
+  document.getElementById("welcome-page-div").style.display = "none"
+
+  const divs = document.getElementsByClassName("content");
+  Array.from(divs).forEach(div => div.style.display = (div.id === "projects-div") ? "grid" : "none");
+}
+
+function backToMain() {
+  document.getElementById("content-div").style.display = "none"
+  document.getElementById("welcome-page-div").style.display = "flex"
+}
+
+function changePage(divId) {
+  const divs = document.getElementsByClassName("content");
+  Array.from(divs).forEach(div => div.style.display = (div.id === divId) ? "grid" : "none");
+}
